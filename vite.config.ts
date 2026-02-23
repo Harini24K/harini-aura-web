3804c65207584e85cdabfb360c098f5060df0465
harini <harinik381@gmail.com>
2026-02-23 18:27:39 +0530

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/harini-aura-web/",
   server: {
     host: "::",
     port: 8080,
